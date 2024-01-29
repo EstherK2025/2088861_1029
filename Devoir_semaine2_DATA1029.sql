b7abe102daeea36d0ed3a070dbf67f745dec6fed
EstherK2025 <estherkipre13@gmail.com>
2024-01-29 15:48:13 -0400

    solution exercice 4

--- Devoir_semaine2_DATA1029.sql
+++ Devoir_semaine2_DATA1029.sql
@@ -14,3 +14,8 @@ WHERE title LIKE '%computer%';
 SELECT title, price
 FROM titles
 WHERE title LIKE "su%" OR title LIKE "bu%";
+
+-- Question4
+SELECT title, price
+FROM titles
+WHERE NOT (title LIKE "su%" OR title LIKE "bu%");
