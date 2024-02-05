1c0df593027f41fb6cd005c6aa4828b8b00c1a16
EstherK2025 <estherkipre13@gmail.com>
2024-02-05 18:40:31 -0400

    solution exercice 4

--- Devoir_semaine3_DATA1029.sql
+++ Devoir_semaine3_DATA1029.sql
@@ -15,10 +15,19 @@ FROM authors A
 	LEFT JOIN titles T ON TA.title_id = T.title_id
 	LEFT JOIN publishers P ON T.pub_id = P.pub_id AND A.city = P.city;
 
--- Question 3 - 
+-- Question 3 - La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les éditeurs qui ne répondent pas à ce critère:
 SELECT A.au_fname, A.au_id, A.city,A.au_fname, A.au_lname AS au_city, P.pub_name,P.pub_id, P.city AS pub_city
 FROM authors A
 	LEFT JOIN titleauthor TA ON A.au_id = TA.au_id
 	LEFT JOIN titles T ON TA.title_id = T.title_id
 	LEFT JOIN publishers P ON T.pub_id = P.pub_id;
+
+-- Question 4 - La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant les auteurs et les éditeurs qui ne répondent pas à ce critère.
+SELECT A.au_lname, A.au_fname, A.au_id, A.city AS au_city, P.pub_name, P.pub_id, P.city AS pub_city
+FROM authors A
+	LEFT JOIN titleauthor TA ON A.au_id = TA.au_id
+	LEFT JOIN titles T ON TA.title_id = T.title_id
+	LEFT JOIN publishers P ON T.pub_id = P.pub_id AND A.city = P.city
+	WHERE NOT A.city = P.city
+
 
