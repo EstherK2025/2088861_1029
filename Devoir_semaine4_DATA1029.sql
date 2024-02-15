65a62ca8c1efcd6134d7a13b99cab4650503ff87
EstherK2025 <estherkipre13@gmail.com>
2024-02-14 21:58:06 -0400

    solution de la question 2

--- Devoir_semaine4_DATA1029.sql
+++ Devoir_semaine4_DATA1029.sql
@@ -16,3 +16,16 @@ WHERE au_id IN (
     )
   )
 );
+
+-- Question 2: Obtenir la liste des auteurs dont l’éditeur «Eyrolles » a publié tous les livres
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id NOT IN (
+  SELECT au_id
+  FROM titles
+  WHERE pub_id = (
+    SELECT pub_id
+    FROM publishers
+    WHERE pub_name = "Harmattan"
+  )
+);
