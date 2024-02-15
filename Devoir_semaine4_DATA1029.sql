2b0d2b8fd228b973b8f76755c3fcdff30a149fec
EstherK2025 <estherkipre13@gmail.com>
2024-02-14 22:10:47 -0400

    solution de la question 1

--- Devoir_semaine4_DATA1029.sql
+++ Devoir_semaine4_DATA1029.sql
@@ -1,23 +1,6 @@
 -- use library_08_02_24;
 
 -- Question 1: Obtenir la liste des auteurs dont l’éditeur « Harmattan » n’a publié aucun livre
-SELECT au_lname, au_fname
-FROM authors
-WHERE au_id IN (
-  SELECT au_id
-  FROM titles
-  WHERE advance > ALL (
-    SELECT advance
-    FROM titles
-    WHERE pub_id = (
-      SELECT pub_id
-      FROM publishers
-      WHERE pub_name = "Harmattan"
-    )
-  )
-);
-
--- Question 2: Obtenir la liste des auteurs dont l’éditeur «Eyrolles » a publié tous les livres
 SELECT au_fname, au_lname
 FROM authors
 WHERE au_id NOT IN (
@@ -30,7 +13,7 @@ WHERE au_id NOT IN (
   )
 );
 
--- Question 3: Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan"
+-- Question 2: Obtenir la liste des auteurs dont l’éditeur «Eyrolles » a publié tous les livres
 SELECT au_fname, au_lname
 FROM authors
 WHERE au_id IN (
@@ -41,6 +24,23 @@ WHERE au_id IN (
     FROM publishers
     WHERE pub_name = "Eyrolles"
   ) AND advance IN (
+    SELECT advance
+    FROM titles
+    WHERE pub_id = (
+      SELECT pub_id
+      FROM publishers
+      WHERE pub_name = "Harmattan"
+    )
+  )
+);
+
+-- Question 3: Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan"
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id IN (
+  SELECT au_id
+  FROM titles
+  WHERE advance > ALL (
     SELECT advance
     FROM titles
     WHERE pub_id = (
