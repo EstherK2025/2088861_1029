a56c6a4bb3680de12cc3cbc2d59165b42cdbd4fc
EstherK2025 <estherkipre13@gmail.com>
2024-02-14 22:35:14 -0400

    solution de la question 3

--- Devoir_semaine4_DATA1029.sql
+++ Devoir_semaine4_DATA1029.sql
@@ -36,4 +36,20 @@ WHERE au_id IN (
   )
 );
 
+-- Question 3: Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan"
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id IN (
+  SELECT au_id
+  FROM titles
+  WHERE advance > ALL (
+    SELECT advance
+    FROM titles
+    WHERE pub_id = (
+      SELECT pub_id
+      FROM publishers
+      WHERE pub_name = "Harmattan"
+    )
+  )
+);
 
